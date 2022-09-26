d7118ba45bba353fbf688383ed32234cbf735a4f
parajwalgaire617 <prajwalgaire617@gmail.com>
2022-09-26 07:37:32 -0400

    queue

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,27 +1,42 @@
+ 
 #include <iostream>
-#include "List.h"
+#include <string>
+
+#include "ArrayQueue.h"
+#include "LinkedListQueue.h"
 using namespace std;
 
-int main(int argc, char** argv){
-    List *list=new List();
-    list->isEmpty();
-    list->addToHead(8);
-    list->addToHead(11);
-    list->addToTail(12);
-    list->addToTail(18);
+int main(){
+
+   LinkedListQueue<int>queue(5);
+   queue.enqueue(10);
+   queue.enqueue(20);
+   queue.enqueue(30);
+   queue.enqueue(40);
+   queue.enqueue(50);
+   queue.enqueue(50);
+   queue.dequeue();
+   cout<<queue.front()<<endl;
+   cout<<queue.back()<<endl;
+   queue.enqueue(60);
+   cout<<queue.isEmpty()<<endl;
+   cout<<queue.isFull()<<endl;
 
 
 
+   ArrayQueue<string>queue2;
+   queue2.dequeue();
+   queue2.enqueue("ram");
+   queue2.enqueue("john");
+   queue2.enqueue("cena");
+   queue2.enqueue("randy");
+   queue2.enqueue("orton");
+   queue2.dequeue();
+   cout<<queue2.front()<<endl;
+   cout<<queue2.back()<<endl;
+   cout<<queue2.isEmpty()<<endl;
+   cout<<queue2.isFull()<<endl;
 
-    Node *predecesor=list->retrieve(7);
-    list->add(15,predecesor);
 
-    list->traversal();
-
-
-    cout<<(list->Search(2)==1?"true data found":"false not found")<<endl;
-    list->removeFromHead();
-    list->isEmpty();
 
 }
-
